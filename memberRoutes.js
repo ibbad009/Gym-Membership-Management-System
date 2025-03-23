4d72d21968485215319dd278acf8df7ce8ce9f86
Ebad Hassan <ebadhassan009@gmail.com>
2025-03-23 09:29:24 +0500

    Update memberRoutes.js

--- memberRoutes.js
+++ memberRoutes.js
@@ -56,17 +56,4 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-// Record attendance
-router.post('/:id/attendance', async (req, res) => {
-  try {
-    const member = await Member.findById(req.params.id);
-    if (!member) return res.status(404).json({ message: 'Member not found' });
-    member.attendance.push(req.body.date);
-    await member.save();
-    res.json(member);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
 module.exports = router;
